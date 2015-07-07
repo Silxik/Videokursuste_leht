ae73e893f03fbbaa7899ce8e21219b47c0dac613
Raul <raul.muser@gmail.com>
2015-07-07 17:18:18 +0300

    WIP: Add admin video adding view

---
+++ controllers/user.php
@@ -0,0 +1,11 @@
+<?php
+
+class user extends Controller
+{
+    //public $requires_auth = true;
+
+    function index()
+    {
+    }
+
+}
---
+++ views/user/user_index.php
@@ -0,0 +1,52 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="well well-sm">
+                <form class="form-horizontal" action="" method="post">
+                    <fieldset>
+                        <legend class="text-center">Lisa video</legend>
+
+                        <!-- Title -->
+                        <div class="form-group">
+                            <label class="col-md-3 control-label" for="title">Pealkiri</label>
+                            <div class="col-md-9">
+                                <input id="title" name="name" type="text" placeholder="Pealkiri" class="form-control">
+                            </div>
+                        </div>
+
+                        <!-- Link-->
+                        <div class="form-group">
+                            <label class="col-md-3 control-label" for="email">Link</label>
+                            <div class="col-md-9">
+                                <input id="link" name="link" type="text" placeholder="Link" class="form-control">
+                            </div>
+                        </div>
+
+                        <!-- Tags -->
+                        <div class="form-group">
+                            <label class="col-md-3 control-label" for="tags">Märksõnad</label>
+                            <div class="col-md-9">
+                                <textarea class="form-control" id="tags" name="tags" placeholder="Märksõnad, eraldatud komaga" rows="3"></textarea>
+                            </div>
+                        </div>
+
+                        <!-- Description -->
+                        <div class="form-group">
+                            <label class="col-md-3 control-label" for="description">Kirjeldus</label>
+                            <div class="col-md-9">
+                                <textarea class="form-control" id="description" name="description" placeholder="Kirjeldus" rows="5"></textarea>
+                            </div>
+                        </div>
+
+                        <!-- Form actions -->
+                        <div class="form-group">
+                            <div class="col-md-12 text-right">
+                                <button type="submit" class="btn btn-primary btn-lg">Lisa</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
