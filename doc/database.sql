246d1cf5f542c5bd2ac84263d96efdd036de2bc6
Raul <raul.muser@gmail.com>
2015-07-08 10:25:31 +0300

    WIP: Add database dump

--- doc/database.sql
+++ doc/database.sql
@@ -1,24 +1,24 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.0-dev
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jul 06, 2015 at 06:17 PM
--- Server version: 5.6.19-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.9
+-- Host: 127.0.0.1
+-- Loomise aeg: Juuli 08, 2015 kell 09:24 EL
+-- Serveri versioon: 5.6.24
+-- PHP versioon: 5.6.8
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `videokursused`
+-- Andmebaas: `videokursuste leht`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `person`
+-- Tabeli struktuur tabelile `person`
 --
 
 DROP TABLE IF EXISTS `person`;
@@ -39,17 +39,17 @@ CREATE TABLE IF NOT EXISTS `person` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `person`
+-- Andmete tõmmistamine tabelile `person`
 --
 
 INSERT INTO `person` (`person_id`, `username`, `person_firstname`, `person_lastname`, `person_first_visit`, `person_last_visit`, `person_SID`, `is_admin`, `password`, `active`, `email`, `setup`, `deleted`) VALUES
-(1, 'demo', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 0, 'demo', 1, '', 0, 0),
-(2, 'henno.taht', 'Henno', 'Täht', '2015-07-06 15:02:15', '2015-07-06 18:05:18', '2e07cc7', 0, '', 0, '', 0, 0);
+  (1, 'demo', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 1, 'demo', 1, '', 0, 0),
+  (2, 'henno.taht', 'Henno', 'Täht', '2015-07-06 15:02:15', '2015-07-06 18:05:18', '2e07cc7', 0, '', 0, '', 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `video`
+-- Tabeli struktuur tabelile `video`
 --
 
 DROP TABLE IF EXISTS `video`;
@@ -63,52 +63,50 @@ CREATE TABLE IF NOT EXISTS `video` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `video`
+-- Andmete tõmmistamine tabelile `video`
 --
 
 INSERT INTO `video` (`video_id`, `title`, `description`, `link`, `date_added`, `person_id`) VALUES
-(1, 'Top 10 SyFy Channel Shows', 'Blurring the line between fantasy and reality, natural and supernatural, normal and paranormal, these are the shows that defined a network. ', 'https://www.youtube.com/watch?v=CPSI9sLxIew', '2015-07-02 12:52:21', 1),
-(2, 'I''M NOT AN ANIMATOR', 'TOMSKA YOU ARE MY FAVOURITE ANIMATOR! NOOOO.', 'https://www.youtube.com/watch?v=mcrsyXyd34A', '2015-07-02 13:14:02', 1);
+  (1, 'Top 10 SyFy Channel Shows', 'Blurring the line between fantasy and reality, natural and supernatural, normal and paranormal, these are the shows that defined a network. ', 'https://www.youtube.com/watch?v=CPSI9sLxIew', '2015-07-02 12:52:21', 1),
+  (2, 'I''M NOT AN ANIMATOR', 'TOMSKA YOU ARE MY FAVOURITE ANIMATOR! NOOOO.', 'https://www.youtube.com/watch?v=mcrsyXyd34A', '2015-07-02 13:14:02', 1);
 
 --
--- Indexes for dumped tables
+-- Indeksid tõmmistatud tabelitele
 --
 
 --
--- Indexes for table `person`
+-- Indeksid tabelile `person`
 --
 ALTER TABLE `person`
-  ADD PRIMARY KEY (`person_id`),
-  ADD UNIQUE KEY `UNIQUE` (`username`);
+ADD PRIMARY KEY (`person_id`), ADD UNIQUE KEY `UNIQUE` (`username`);
 
 --
--- Indexes for table `video`
+-- Indeksid tabelile `video`
 --
 ALTER TABLE `video`
-  ADD PRIMARY KEY (`video_id`),
-  ADD KEY `user_id` (`person_id`);
+ADD PRIMARY KEY (`video_id`), ADD KEY `user_id` (`person_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT tõmmistatud tabelitele
 --
 
 --
--- AUTO_INCREMENT for table `person`
+-- AUTO_INCREMENT tabelile `person`
 --
 ALTER TABLE `person`
-  MODIFY `person_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `person_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT for table `video`
+-- AUTO_INCREMENT tabelile `video`
 --
 ALTER TABLE `video`
-  MODIFY `video_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `video_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- Constraints for dumped tables
+-- Tõmmistatud tabelite piirangud
 --
 
 --
--- Constraints for table `video`
+-- Piirangud tabelile `video`
 --
 ALTER TABLE `video`
-  ADD CONSTRAINT `video_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`);
+ADD CONSTRAINT `video_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`);
 SET FOREIGN_KEY_CHECKS=1;
