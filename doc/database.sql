e2de0fb5e37a2c577d238463158b15dff1dc3933
Raul <raul.muser@gmail.com>
2015-07-13 22:33:02 +0300

    Update database

--- doc/database.sql
+++ doc/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Loomise aeg: Juuli 13, 2015 kell 04:09 PL
+-- Loomise aeg: Juuli 13, 2015 kell 09:32 PL
 -- Serveri versioon: 5.6.24
 -- PHP versioon: 5.6.8
 
@@ -73,49 +73,57 @@ DROP TABLE IF EXISTS `tag`;
 CREATE TABLE IF NOT EXISTS `tag` (
   `tag_id` int(10) unsigned NOT NULL,
   `tag_name` varchar(155) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8;
 
 --
 -- Andmete tõmmistamine tabelile `tag`
 --
 
 INSERT INTO `tag` (`tag_id`, `tag_name`) VALUES
-  (5, 'mysql'),
-  (6, 'php'),
-  (7, 'tutorial'),
-  (8, 'Software'),
-  (9, 'Helping develop'),
-  (10, 'search'),
-  (11, 'php search'),
-  (12, 'make'),
-  (13, 'custom'),
-  (14, 'helpingdevelop'),
-  (15, 'free'),
-  (16, 'source'),
-  (17, 'html'),
-  (18, 'Joe'),
-  (19, 'Smith'),
-  (20, 'tutor'),
-  (21, 'Twitter Bootstrap'),
-  (22, 'Bootstrap 3'),
-  (23, 'Web Design'),
-  (24, 'Web Design Tutorial'),
-  (25, 'Udemy'),
-  (26, 'Udemy course'),
-  (27, 'udemy web design'),
-  (28, 'bootstrap tutorial'),
-  (29, 'responsive web design'),
-  (30, 'PHP (Programming Language)'),
-  (31, 'learn to code'),
-  (32, 'brad hussey'),
-  (33, 'udemy courses'),
-  (34, 'free tutorials'),
-  (35, 'free programming tutorial'),
-  (36, 'freebie'),
-  (37, 'learn php'),
-  (38, 'web development'),
-  (39, 'Dynamic Web Page'),
-  (40, 'Free');
+  (1, 'Twitter Bootstrap'),
+  (2, 'Bootstrap 3'),
+  (3, 'Web Design'),
+  (4, 'Web Design Tutorial'),
+  (5, 'Udemy'),
+  (6, 'Udemy course'),
+  (7, 'udemy web design'),
+  (8, 'bootstrap tutorial'),
+  (9, 'responsive web design'),
+  (10, 'PHP (Programming Language)'),
+  (11, 'learn to code'),
+  (12, 'brad hussey'),
+  (13, 'udemy courses'),
+  (14, 'free tutorials'),
+  (15, 'free programming tutorial'),
+  (16, 'freebie'),
+  (17, 'learn php'),
+  (18, 'web development'),
+  (19, 'Dynamic Web Page'),
+  (20, 'Free'),
+  (21, 'Helping develop'),
+  (22, 'php'),
+  (23, 'search'),
+  (24, 'php search'),
+  (25, 'make'),
+  (26, 'custom'),
+  (27, 'helpingdevelop'),
+  (28, 'free'),
+  (29, 'source'),
+  (30, 'mysql'),
+  (31, 'html'),
+  (32, 'Joe'),
+  (33, 'Smith'),
+  (34, 'tutor'),
+  (35, 'tutorial'),
+  (36, 'PHP MySQL Tutorial'),
+  (37, 'MySQL Prepared Statements'),
+  (38, 'PHP Prepared Statements'),
+  (39, 'MySQL (Software)'),
+  (40, 'jquery'),
+  (41, 'ajax'),
+  (42, 'javascript'),
+  (43, 'iviewsource'),
+  (44, 'front end development');
 
 -- --------------------------------------------------------
 
@@ -139,10 +147,11 @@ CREATE TABLE IF NOT EXISTS `video` (
 --
 
 INSERT INTO `video` (`video_id`, `title`, `desc`, `link`, `date_added`, `person_id`, `public`) VALUES
-  (2, 'PHP Tutorials: How to filter data from a MySQL Database Table with PHP', 'Hello everyone. This is another PHP / MySQL tutorial about how to filter data from a MySQL Database Table using PHP. As always, i have to apologize for my bad English. Feel free to mute the audio and watch the tutorial instead. I hope you find this tutori', 'https://www.youtube.com/watch?v=9J3S60VRYlE', '2015-07-13 14:03:28', 1, 1),
-  (3, 'Creating a PHP Search', 'This is a tutorial which goes over how to create a PHP search which filters out results from a database table. Sorry for the mistakes made in this video, the video following this goes over how to take this and make it instant with jQuery\r\n\r\nTutor Facebook', 'https://www.youtube.com/watch?v=PBLuP2JZcEg', '2015-07-13 14:04:30', 1, 1),
-  (4, 'Code a Responsive Website with Bootstrap 3 - [Lecture 1] Welcome & What is Bootstrap 3?', 'LECTURE 1\r\nIn this lecture, we briefly cover what Bootstrap 3 is and why it is different than the older versions of Twitter Bootstrap.\r\n\r\nDOWNLOAD FINAL COURSE FILES\r\nhttp://bradhussey.ca/bootstrap-course-files\r\n\r\nBUY ME A LATTÉ\r\nIf this course has helped', 'https://www.youtube.com/watch?v=oepmLGQP1m4', '2015-07-13 14:04:59', 1, 1),
-  (5, 'Code Dynamic Websites with PHP [#1]', 'Hey everybody, welcome to Code Dynamic Websites with PHP!\r\n\r\nMy name is Brad Hussey and for the next little while, I''m going to be your personal instructor, and I will be teaching you how to hand code PHP!\r\n\r\nThis course is a Total Beginner''s Guide to Cod', 'https://www.youtube.com/watch?v=vJfXVAYSw04', '2015-07-13 14:05:09', 1, 1);
+  (1, 'Code a Responsive Website with Bootstrap 3 - [Lecture 1] Welcome & What is Bootstrap 3?', 'LECTURE 1\r\nIn this lecture, we briefly cover what Bootstrap 3 is and why it is different than the older versions of Twitter Bootstrap.\r\n\r\nDOWNLOAD FINAL COURSE FILES\r\nhttp://bradhussey.ca/bootstrap-course-files\r\n\r\nBUY ME A LATTÉ\r\nIf this course has helped', 'https://www.youtube.com/watch?v=oepmLGQP1m4', '2015-07-13 19:20:52', 1, 1),
+  (2, 'Code Dynamic Websites with PHP [#1]', 'Hey everybody, welcome to Code Dynamic Websites with PHP!\r\n\r\nMy name is Brad Hussey and for the next little while, I''m going to be your personal instructor, and I will be teaching you how to hand code PHP!\r\n\r\nThis course is a Total Beginner''s Guide to Cod', 'https://www.youtube.com/watch?v=vJfXVAYSw04', '2015-07-13 19:21:28', 1, 1),
+  (3, 'Creating a PHP Search', 'This is a tutorial which goes over how to create a PHP search which filters out results from a database table. Sorry for the mistakes made in this video, the video following this goes over how to take this and make it instant with jQuery\r\n\r\nTutor Facebook', 'https://www.youtube.com/watch?v=PBLuP2JZcEg', '2015-07-13 19:22:41', 1, 1),
+  (4, 'PHP MySQL Tutorial', 'Get the Code Here : http://goo.gl/Aocylf\r\n\r\nPHP Tutorial : https://www.youtube.com/watch?v=7TF00hJI78Y\r\n\r\nMySQL Tutorial : https://www.youtube.com/watch?v=yPu6qV5byu4\r\n\r\nPreviously I covered the vast majority of both PHP and MySQL in 2 videos. This time I', 'https://www.youtube.com/watch?v=mpQts3ezPVg', '2015-07-13 19:25:18', 1, 1),
+  (5, 'Learning how to use jQuery AJAX with PHP', 'Getting started with AJAX is super easy when you use the jQuery library. That works well for the client side, but how do you work with a server side language like PHP? It''s easier than you think.', 'https://www.youtube.com/watch?v=TR0gkGbMwW0', '2015-07-13 19:25:47', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -161,45 +170,53 @@ CREATE TABLE IF NOT EXISTS `video_tags` (
 --
 
 INSERT INTO `video_tags` (`video_id`, `tag_id`) VALUES
-  (2, 5),
-  (3, 5),
-  (2, 6),
-  (3, 6),
-  (2, 7),
-  (3, 7),
-  (2, 8),
-  (3, 9),
-  (3, 10),
-  (3, 11),
-  (3, 12),
-  (3, 13),
-  (3, 14),
-  (3, 15),
-  (5, 15),
-  (3, 16),
-  (3, 17),
-  (3, 18),
-  (3, 19),
+  (1, 1),
+  (1, 2),
+  (1, 3),
+  (1, 4),
+  (1, 5),
+  (1, 6),
+  (1, 7),
+  (1, 8),
+  (1, 9),
+  (2, 10),
+  (4, 10),
+  (2, 11),
+  (2, 12),
+  (2, 13),
+  (2, 14),
+  (2, 15),
+  (2, 16),
+  (2, 17),
+  (2, 18),
+  (2, 19),
+  (2, 20),
   (3, 20),
-  (4, 21),
-  (4, 22),
-  (4, 23),
-  (4, 24),
-  (4, 25),
-  (4, 26),
-  (4, 27),
-  (4, 28),
-  (4, 29),
-  (5, 30),
-  (5, 31),
-  (5, 32),
-  (5, 33),
-  (5, 34),
+  (3, 21),
+  (3, 22),
+  (5, 22),
+  (3, 23),
+  (3, 24),
+  (3, 25),
+  (3, 26),
+  (3, 27),
+  (3, 29),
+  (3, 30),
+  (3, 31),
+  (3, 32),
+  (3, 33),
+  (3, 34),
+  (3, 35),
   (5, 35),
-  (5, 36),
-  (5, 37),
-  (5, 38),
-  (5, 39);
+  (4, 36),
+  (4, 37),
+  (4, 38),
+  (4, 39),
+  (5, 40),
+  (5, 41),
+  (5, 42),
+  (5, 43),
+  (5, 44);
 
 --
 -- Indeksid tõmmistatud tabelitele
@@ -253,7 +270,7 @@ MODIFY `person_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- AUTO_INCREMENT tabelile `tag`
 --
 ALTER TABLE `tag`
-MODIFY `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=41;
+MODIFY `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=45;
 --
 -- AUTO_INCREMENT tabelile `video`
 --
