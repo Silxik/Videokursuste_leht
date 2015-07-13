752597b48f537eb507294dcb9b58f92ce9e90513
Raul <raul.muser@gmail.com>
2015-07-13 17:09:51 +0300

    Update database

--- doc/database.sql
+++ doc/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Loomise aeg: Juuli 13, 2015 kell 12:22 PL
+-- Loomise aeg: Juuli 13, 2015 kell 04:09 PL
 -- Serveri versioon: 5.6.24
 -- PHP versioon: 5.6.8
 
@@ -29,27 +29,7 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `date_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `video_id` int(11) unsigned NOT NULL,
   `person_id` int(11) unsigned NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
-
---
--- Andmete tõmmistamine tabelile `comment`
---
-
-INSERT INTO `comment` (`comment_id`, `comment`, `rating`, `date_added`, `video_id`, `person_id`) VALUES
-  (1, 'Lahe video!', 5, '2015-07-11 16:26:10', 2, 3),
-  (2, 'Huvitav video!', 5, '2015-07-11 16:26:10', 1, 1),
-  (3, 'Animators UNITE!!!', 5, '2015-07-11 16:26:10', 2, 1),
-  (4, 'Good to know!', 5, '2015-07-11 16:26:10', 2, 1),
-  (5, 'I wish to be such good animator', 5, '2015-07-11 16:26:10', 2, 1),
-  (6, 'Animator test', 5, '2015-07-11 16:26:10', 2, 1),
-  (7, 'ALL BOW TO MASTER ANIMATOR!', 5, '2015-07-11 16:27:04', 2, 1),
-  (8, 'I am here to troll and chew bubblegum... And I am all out of bubble gum! THIS VIDEO SUCKS, YOU ARE JUST LAZY!', 5, '2015-07-11 16:30:35', 2, 1),
-  (9, 'half-life 3 confirmed', 5, '2015-07-11 17:33:34', 2, 3),
-  (10, 'Kommentaar', 5, '2015-07-13 07:24:26', 2, 1),
-  (11, 'Test2', 5, '2015-07-13 07:28:59', 2, 1),
-  (12, 'kolm', 5, '2015-07-13 07:30:24', 2, 1),
-  (13, 'neljas katse', 5, '2015-07-13 07:53:17', 2, 1),
-  (14, 'Mina ka!', 5, '2015-07-13 07:53:47', 2, 3);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -92,18 +72,50 @@ INSERT INTO `person` (`person_id`, `username`, `person_firstname`, `person_lastn
 DROP TABLE IF EXISTS `tag`;
 CREATE TABLE IF NOT EXISTS `tag` (
   `tag_id` int(10) unsigned NOT NULL,
-  `tag_name` varchar(25) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `tag_name` varchar(155) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
 
 --
 -- Andmete tõmmistamine tabelile `tag`
 --
 
 INSERT INTO `tag` (`tag_id`, `tag_name`) VALUES
-  (1, 'Top 10'),
-  (2, 'Animator'),
-  (3, 'WatchMojo'),
-  (4, 'TomSka');
+  (5, 'mysql'),
+  (6, 'php'),
+  (7, 'tutorial'),
+  (8, 'Software'),
+  (9, 'Helping develop'),
+  (10, 'search'),
+  (11, 'php search'),
+  (12, 'make'),
+  (13, 'custom'),
+  (14, 'helpingdevelop'),
+  (15, 'free'),
+  (16, 'source'),
+  (17, 'html'),
+  (18, 'Joe'),
+  (19, 'Smith'),
+  (20, 'tutor'),
+  (21, 'Twitter Bootstrap'),
+  (22, 'Bootstrap 3'),
+  (23, 'Web Design'),
+  (24, 'Web Design Tutorial'),
+  (25, 'Udemy'),
+  (26, 'Udemy course'),
+  (27, 'udemy web design'),
+  (28, 'bootstrap tutorial'),
+  (29, 'responsive web design'),
+  (30, 'PHP (Programming Language)'),
+  (31, 'learn to code'),
+  (32, 'brad hussey'),
+  (33, 'udemy courses'),
+  (34, 'free tutorials'),
+  (35, 'free programming tutorial'),
+  (36, 'freebie'),
+  (37, 'learn php'),
+  (38, 'web development'),
+  (39, 'Dynamic Web Page'),
+  (40, 'Free');
 
 -- --------------------------------------------------------
 
@@ -114,23 +126,23 @@ INSERT INTO `tag` (`tag_id`, `tag_name`) VALUES
 DROP TABLE IF EXISTS `video`;
 CREATE TABLE IF NOT EXISTS `video` (
   `video_id` int(10) unsigned NOT NULL,
-  `title` varchar(50) NOT NULL,
+  `title` varchar(155) NOT NULL,
   `desc` varchar(255) NOT NULL,
   `link` varchar(155) NOT NULL,
   `date_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `person_id` int(10) unsigned NOT NULL,
-  `public` tinyint(1) NOT NULL DEFAULT '1',
-  `tags` varchar(64) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `public` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Andmete tõmmistamine tabelile `video`
 --
 
-INSERT INTO `video` (`video_id`, `title`, `desc`, `link`, `date_added`, `person_id`, `public`, `tags`) VALUES
-  (1, 'Top 10 SyFy Channel Shows', 'Blurring the line between fantasy and reality, natural and supernatural, normal and paranormal, these are the shows that defined a network. ', 'https://www.youtube.com/watch?v=CPSI9sLxIew', '2015-07-02 12:52:21', 1, 1, ''),
-  (2, 'I''M NOT AN ANIMATOR', 'TOMSKA YOU ARE MY FAVOURITE ANIMATOR! NOOOO.', 'https://www.youtube.com/watch?v=mcrsyXyd34A', '2015-07-02 13:14:02', 1, 1, ''),
-  (3, 'asdfmovie 1-8 (Complete Collection)', 'every asdfmovie so far! oh my!!!', 'http://www.youtube.com/watch?v=PF9z-kEmic4', '2015-07-13 10:04:18', 3, 1, '');
+INSERT INTO `video` (`video_id`, `title`, `desc`, `link`, `date_added`, `person_id`, `public`) VALUES
+  (2, 'PHP Tutorials: How to filter data from a MySQL Database Table with PHP', 'Hello everyone. This is another PHP / MySQL tutorial about how to filter data from a MySQL Database Table using PHP. As always, i have to apologize for my bad English. Feel free to mute the audio and watch the tutorial instead. I hope you find this tutori', 'https://www.youtube.com/watch?v=9J3S60VRYlE', '2015-07-13 14:03:28', 1, 1),
+  (3, 'Creating a PHP Search', 'This is a tutorial which goes over how to create a PHP search which filters out results from a database table. Sorry for the mistakes made in this video, the video following this goes over how to take this and make it instant with jQuery\r\n\r\nTutor Facebook', 'https://www.youtube.com/watch?v=PBLuP2JZcEg', '2015-07-13 14:04:30', 1, 1),
+  (4, 'Code a Responsive Website with Bootstrap 3 - [Lecture 1] Welcome & What is Bootstrap 3?', 'LECTURE 1\r\nIn this lecture, we briefly cover what Bootstrap 3 is and why it is different than the older versions of Twitter Bootstrap.\r\n\r\nDOWNLOAD FINAL COURSE FILES\r\nhttp://bradhussey.ca/bootstrap-course-files\r\n\r\nBUY ME A LATTÉ\r\nIf this course has helped', 'https://www.youtube.com/watch?v=oepmLGQP1m4', '2015-07-13 14:04:59', 1, 1),
+  (5, 'Code Dynamic Websites with PHP [#1]', 'Hey everybody, welcome to Code Dynamic Websites with PHP!\r\n\r\nMy name is Brad Hussey and for the next little while, I''m going to be your personal instructor, and I will be teaching you how to hand code PHP!\r\n\r\nThis course is a Total Beginner''s Guide to Cod', 'https://www.youtube.com/watch?v=vJfXVAYSw04', '2015-07-13 14:05:09', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -149,12 +161,45 @@ CREATE TABLE IF NOT EXISTS `video_tags` (
 --
 
 INSERT INTO `video_tags` (`video_id`, `tag_id`) VALUES
-  (1, 1),
-  (2, 2),
-  (3, 2),
-  (1, 3),
-  (2, 4),
-  (3, 4);
+  (2, 5),
+  (3, 5),
+  (2, 6),
+  (3, 6),
+  (2, 7),
+  (3, 7),
+  (2, 8),
+  (3, 9),
+  (3, 10),
+  (3, 11),
+  (3, 12),
+  (3, 13),
+  (3, 14),
+  (3, 15),
+  (5, 15),
+  (3, 16),
+  (3, 17),
+  (3, 18),
+  (3, 19),
+  (3, 20),
+  (4, 21),
+  (4, 22),
+  (4, 23),
+  (4, 24),
+  (4, 25),
+  (4, 26),
+  (4, 27),
+  (4, 28),
+  (4, 29),
+  (5, 30),
+  (5, 31),
+  (5, 32),
+  (5, 33),
+  (5, 34),
+  (5, 35),
+  (5, 36),
+  (5, 37),
+  (5, 38),
+  (5, 39);
 
 --
 -- Indeksid tõmmistatud tabelitele
@@ -198,7 +243,7 @@ ADD PRIMARY KEY (`video_id`,`tag_id`), ADD KEY `tag_id` (`tag_id`);
 -- AUTO_INCREMENT tabelile `comment`
 --
 ALTER TABLE `comment`
-MODIFY `comment_id` int(155) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
+MODIFY `comment_id` int(155) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT tabelile `person`
 --
@@ -208,12 +253,12 @@ MODIFY `person_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- AUTO_INCREMENT tabelile `tag`
 --
 ALTER TABLE `tag`
-MODIFY `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=41;
 --
 -- AUTO_INCREMENT tabelile `video`
 --
 ALTER TABLE `video`
-MODIFY `video_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `video_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- Tõmmistatud tabelite piirangud
 --
